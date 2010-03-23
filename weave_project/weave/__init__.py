c69b791b02a66e9e35869c48dd4e132f19243518
Jens Diemer <git@jensdiemer.de>
2010-03-23 13:39:13 +0000

    change version string format (SVN reversion)

--- weave_project/weave/__init__.py
+++ weave_project/weave/__init__.py
@@ -12,12 +12,16 @@ except ImportError:
 else:
     path = os.path.split(os.path.abspath(__file__))[0]
     svn_revision = get_svn_revision(path)
+    if svn_revision == u'SVN-unknown':
+        svn_revision = ""
+    else:
+        svn_revision = svn_revision.replace("-", "").lower()
 
 
 __version__ = (0, 1, 3, svn_revision)
 __api__ = (1, 0)
 
-VERSION_STRING = "%i.%i.%i %s" % __version__
+VERSION_STRING = "%i.%i.%i.%s" % __version__
 API_STRING = '.'.join(str(integer) for integer in __api__)
 
 
