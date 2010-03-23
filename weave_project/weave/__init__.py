3119e0134967245b1c27992714999f149a575875
Jens Diemer <git@jensdiemer.de>
2010-03-23 14:27:25 +0000

    bugfix if SVN version is not available.

--- weave_project/weave/__init__.py
+++ weave_project/weave/__init__.py
@@ -5,23 +5,23 @@ import os
 import logging
 
 
+__version__ = (0, 1, 3)
+__api__ = (1, 0)
+
+
 try:
     from django.utils.version import get_svn_revision
 except ImportError:
-    svn_revision = ""
+    pass
 else:
     path = os.path.split(os.path.abspath(__file__))[0]
     svn_revision = get_svn_revision(path)
-    if svn_revision == u'SVN-unknown':
-        svn_revision = ""
-    else:
+    if svn_revision != u'SVN-unknown':
         svn_revision = svn_revision.replace("-", "").lower()
+        __version__ += (svn_revision,)
 
 
-__version__ = (0, 1, 3, svn_revision)
-__api__ = (1, 0)
-
-VERSION_STRING = "%i.%i.%i.%s" % __version__
+VERSION_STRING = '.'.join(str(part) for part in __version__)
 API_STRING = '.'.join(str(integer) for integer in __api__)
 
 
