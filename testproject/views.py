b5f274a71b61cc1ca6af228cf297df4fbeae9c07
JensDiemer <git@jensdiemer.de>
2011-06-28 10:59:04 +0200

    sync setup dialog only accept the server url if it's ends with a slash

--- testproject/views.py
+++ testproject/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 
+
 def absolute_uri(request, view_name, **kwargs):
     url = reverse(view_name, kwargs=kwargs)
     absolute_uri = request.build_absolute_uri(url)
@@ -12,9 +13,15 @@ def absolute_uri(request, view_name, **kwargs):
 
 @login_required
 def url_info(request):
+
+    server_url = request.build_absolute_uri("weave")
+    if not server_url.endswith("/"):
+        # sync setup dialog only accept the server url if it's ends with a slash
+        server_url += "/"
+
     context = {
         "title": "weave testproject url info",
-        "server_url": request.build_absolute_uri("weave"),
+        "server_url": server_url,
         "register_check_url": absolute_uri(request, "weave-register_check", username=request.user.username),
         "info_url": absolute_uri(request, "weave-info", version="1.0", username=request.user.username),
     }
