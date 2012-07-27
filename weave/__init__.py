cb8fec531b02a181d24dfa5be723841326d5d14b
JensDiemer <git@jensdiemer.de>
2012-07-27 13:44:18 +0200

    * set version to v0.4.2 * update README

--- weave/__init__.py
+++ weave/__init__.py
@@ -5,7 +5,7 @@
     -build version string
     
     @license: GNU GPL v3 or above, see LICENSE for more details.
-    @copyleft: 2010-2011 by the django-sync-server team, see AUTHORS for more details.
+    @copyleft: 2010-2012 by the django-sync-server team, see AUTHORS for more details.
 """
 
 
@@ -16,17 +16,13 @@ import time
 import warnings
 
 
-__version__ = (0, 4, 1)
+__version__ = (0, 4, 2)
 __api__ = (1, 1)
 
 
 VERSION_STRING = '.'.join(str(part) for part in __version__)
 API_STRING = '.'.join(str(integer) for integer in __api__)
 
-
-# Display get_git_hash() errors as warnings?
-#VERBOSE = True
-VERBOSE = False
 
 
 class Logging(object):
@@ -50,51 +46,6 @@ class Logging(object):
 
         return cls.logger
     get_logger = classmethod(get_logger)
-
-
-
-def _error(msg):
-    if VERBOSE:
-        warnings.warn(msg)
-    return ""
-
-def get_commit_timestamp(path=None):
-    if path is None:
-        path = os.path.abspath(os.path.dirname(__file__))
-
-    try:
-        process = subprocess.Popen(
-            # %ct: committer date, UNIX timestamp  
-            ["/usr/bin/git", "log", "--pretty=format:%ct", "-1", "HEAD"],
-            shell=False, cwd=path,
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-        )
-    except Exception, err:
-        return _error("Can't get git hash: %s" % err)
-
-    process.wait()
-    returncode = process.returncode
-    if returncode != 0:
-        return _error(
-            "Can't get git hash, returncode was: %r"
-            " - git stdout: %r"
-            " - git stderr: %r"
-            % (returncode, process.stdout.readline(), process.stderr.readline())
-        )
-
-    output = process.stdout.readline().strip()
-    try:
-        timestamp = int(output)
-    except Exception, err:
-        return _error("git log output is not a number, output was: %r" % output)
-
-    try:
-        return time.strftime(".%m%d", time.gmtime(timestamp))
-    except Exception, err:
-        return _error("can't convert %r to time string: %s" % (timestamp, err))
-
-
-VERSION_STRING += get_commit_timestamp()
 
 
 if __name__ == "__main__":
